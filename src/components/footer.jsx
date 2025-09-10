7707360545df3960b8540ace23bb650dc92a518d
Kenny Otesile <kennyotesile@gmail.com>
2025-09-10 12:15:31 +0100

    Update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -41,7 +41,7 @@ export default function Footer({
                     </li>
                 </ul>
                 <div className="flex flex-1">
-                    &copy; KENNY OTESILE {(new Date()).getFullYear()}
+                    &copy; KENNY OTESILE CO LIMITED {(new Date()).getFullYear()}
                 </div>
             </div>
         </footer>
